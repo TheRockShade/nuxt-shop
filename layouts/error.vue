65359926827aae8bfc53235071d25b207cd7b174
Vladislav Bykov <therockshade@gmail.com>
2021-05-30 12:12:39 +0400

    add styles for error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,9 @@
 <template>
   <section class="error">
-    <div class="error__inner grid-block">
-      <h1 class="error__title">Oooops...</h1>
-      <p class="error__text">Sorry, page not found :(</p>
+    <div class="error__inner container">
+      <h1 class="error__title">Упс...</h1>
+      <p class="error__text">Страницы не существует</p>
+      <nuxt-link class="error__link" to="/">На главную</nuxt-link>
     </div>
   </section>
 </template>
@@ -17,3 +18,36 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.error {
+  padding: 30vh 0 190px 0;
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__title {
+    margin-bottom: 20px;
+
+    font-size: 60px;
+    font-weight: 500;
+  }
+
+  &__text {
+    margin-bottom: 20px;
+
+    font-size: 24px;
+  }
+
+  &__link {
+    font-size: 24px;
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+}
+</style>
