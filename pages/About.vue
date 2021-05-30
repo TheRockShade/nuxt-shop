e01b72af5a00179593cb4f92c3c397b9db28e76e
Vladislav Bykov <therockshade@gmail.com>
2021-05-30 12:40:50 +0400

    fix about page, add text styles

--- pages/About.vue
+++ pages/About.vue
@@ -1,56 +1,23 @@
 <template>
-  <section class="about">
-    <div class="about__inner container">
-      <h2 class="about__title">О компании</h2>
-      <p class="about__text about__text--bold">
+  <section class="text-styles">
+    <div class="container">
+      <h2>О компании</h2>
+      <p class="bold">
         Интернет-магазин "Mono.ru" – лучшие смартфоны по низким ценам!
       </p>
-      <p class="about__text">
+      <p>
         Мы рады предложить вам недорогие, но качественные товары с подробными
         описаниями, характеристиками и фотографиями. У нас Вы можете купить
         замечательные товары: технику, электронику, одежду, обувь, игрушки,
         книги и многое другое в вашем регионе по ценам производителей и без
         наценки.
       </p>
-      <p class="about__text about__text--bold">Наши главные преимущества:</p>
-      <ul class="about__list">
-        <li class="about__item">Низкие цены от производителей</li>
-        <li class="about__item">Доставка по городу в день заказа</li>
-        <li class="about__item">
-          Доставка заказов Почтой по всей Стране за 5-15 дней
-        </li>
+      <p class="bold">Наши главные преимущества:</p>
+      <ul>
+        <li>Низкие цены от производителей</li>
+        <li>Доставка по городу в день заказа</li>
+        <li>Доставка заказов Почтой по всей Стране за 5-15 дней</li>
       </ul>
     </div>
   </section>
 </template>
-
-<style lang="scss" scoped>
-.about {
-  padding: 60px 0;
-  &__title {
-    margin-bottom: 16px;
-
-    font-size: 24px;
-  }
-
-  &__text {
-    margin-bottom: 16px;
-
-    &--bold {
-      font-weight: 700;
-    }
-  }
-
-  &__list {
-    margin-left: 20px;
-
-    list-style: initial;
-  }
-
-  &__item {
-    &:not(:last-child) {
-      margin-bottom: 10px;
-    }
-  }
-}
-</style>
